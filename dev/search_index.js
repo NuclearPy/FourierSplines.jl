fc3c092ff7f3582fe9930b11cfd45e43f9376261
Documenter.jl <documenter@juliadocs.github.io>
2023-11-02 03:13:18 +0000

    build based on e8ffcd0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FourierSplines","category":"page"},{"location":"#FourierSplines","page":"Home","title":"FourierSplines","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FourierSplines.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FourierSplines]","category":"page"}]
+}
