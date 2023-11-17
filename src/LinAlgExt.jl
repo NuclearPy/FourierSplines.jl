e4cf5ac9b48c25ace6623191e02cc5463ba4262a
Adam Zaidan <adam.k.zaidan@gmail.com>
2023-11-17 10:17:32 -0500

    Added some matrix functionality

--- src/LinAlgExt.jl
+++ src/LinAlgExt.jl
@@ -1,71 +1,103 @@
 mutable struct SplineLinOp{T<:Number}
-    knots::Knots
+    domain::Knots
+    range::Knots
     coefficients::Array{T}
 end
 
+# Utilities and basic operators
+
+# Logic
+Base.:(==)(M1::SplineLinOp, M2::SplineLinOp) = (M1.domain == M2.domain) && (M1.coefficients == M2.coefficients)
+
+# Initialization
+Base.zeros(T::Type, Domain::Knots, Range::Knots) = SplineLinOp(Domain, Range, zeros(T, dimension(Range), dimension(Domain)))
+Base.zeros(T::Type, Domain::Knots, Range::Knots) = SplineLinOp(Domain, Range, ones(T, dimension(Range), dimension(Domain)))
+Base.fill(value::Number, Domain::Knots, Range::Knots) = SplineLinOp(Domain, Range, fill(value, dimension(Range), dimension(Domain)))
+
+# Copying
+Base.copy(M::SplineLinOp) = SplineLinOp(M.domain, M.range, copy(M.coefficients))
+
+# Indexing, sizes, etc
+#Base.parent(S::Spline) = S.coefficients
+#Base.parent(K::Knots) = K.knots
+#Base.length(S::Spline) = length(S.coefficients)
+#Base.size(S::Spline) = size(S.coefficients)
+#Base.getindex(S::Spline, i::Int) = S.coefficients[i]
+#Base.setindex!(S::Spline, val::Number, i::Int) = (S.coefficients[i] = val)
+#Base.axes(S::Spline) = 1:length(S.coefficients)
+
 function Base.:+(M1::SplineLinOp, M2::SplineLinOp)
-    K = M1.knots
-    if K == M2.knots
-        return SplineLinOp(K, M1+M2)
+    K1 = M1.domain
+    K2 = M1.range
+    if (K1 == M2.domain) && (K2 == M2.range)
+        return SplineLinOp(K1, K2, M1+M2)
     else
         return error("Knots must be the same")
     end
 end
 
 function Base.:-(M1::SplineLinOp, M2::SplineLinOp)
-    K = M1.knots
-    if K == M2.knots
-        return SplineLinOp(K, M1-M2)
+    K1 = M1.domain
+    K2 = M1.range
+    if (K1 == M2.domain) && (K2 == M2.range)
+        return SplineLinOp(K1, K2, M1-M2)
     else
         return error("Knots must be the same")
     end
 end
 
-Base.:*(α::Number, M::SplineLinOp) = SplineLinOp(M.knots, α*M.coefficients)
-Base.:*(M::SplineLinOp, α::Number) = SplineLinOp(M.knots, α*M.coefficients)
-Base.:/(M::SplineLinOp, α::Number) = SplineLinOp(M.knots, M.coefficients/α)
+Base.:*(α::Number, M::SplineLinOp) = SplineLinOp(M.domain, M.range, α*M.coefficients)
+Base.:*(M::SplineLinOp, α::Number) = SplineLinOp(M.domain, M.range, α*M.coefficients)
+Base.:/(M::SplineLinOp, α::Number) = SplineLinOp(M.domain, M.range, M.coefficients/α)
 
 function Base.:*(M::SplineLinOp, v::Spline)
-    K = M.knots
+    K = M.domain
     if K == v.knots
-        return SplineLinOp(K, M.coefficients*v.coefficients)
+        return Spline(M.range, M.coefficients*v.coefficients)
     else
         return error("Knots must be the same")
     end
 end
 
 function Base.:*(M1::SplineLinOp, M2::SplineLinOp)
-    K = M1.knots
-    if K == M2.knots
-        return SplineLinOp(K, M1.coefficients*M2.coefficients)
+    K = M1.domain
+    if K == M2.range
+        return SplineLinOp(M2.domain, M1.range, M1.coefficients*M2.coefficients)
     else
         return error("Knots must be the same")
     end
 end
 
 function Base.:\(M::SplineLinOp, v::Spline)
-    K = M.knots
+    K = M.range
     if K == v.knots
-        return SplineLinOp(K, M.coefficients\v.coefficients)
+        return Spline(M.domain, M.coefficients\v.coefficients)
     else
         return error("Knots must be the same")
     end
 end
 
 function Base.:\(M1::SplineLinOp, M2::SplineLinOp)
-    K = M1.knots
-    if K == M2.knots
-        return SplineLinOp(K, M1.coefficients\M2.coefficients)
+    K = M1.range
+    if K == M2.range
+        return SplineLinOp(M2.domain, M1.domain, M1.coefficients\M2.coefficients)
     else
         return error("Knots must be the same")
     end
 end
 
-for f ∈ [:inv, :exp, :sin, :cos, :tan]
-    @eval LinearAlgebra.$f(M::SplineLinOp) = SplineLinOp(M.knots, $f(M.coefficients))
+Base.adjoint(M::SplineLinOp) = SplineLinOp(M.range, M.domain, parent(M'))
+
+Base.:/(M1::SplineLinOp, M2::SplineLinOp) = (M2' \ M1')'
+
+Base.:^(M::SplineLinOp, α::Number) = SplineLinOp(M.domain, M.range, M.coefficients^α)
+
+for f ∈ [:inv, :exp, :sin, :cos, :tan, :sqrt]
+    @eval LinearAlgebra.$f(M::SplineLinOp) = SplineLinOp(M.domain, M.range, $f(M.coefficients))
 end
 
 LinearAlgebra.det(M::SplineLinOp) = det(M.coefficients)
+LinearAlgebra.logdet(M::SplineLinOp) = logdet(M.coefficients)
 
 # Norms and operator norms
 
